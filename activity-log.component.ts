14d14b28f705f07887d1a8ee822c3467aa2c1f77
Dhabale <amruta.dhabale@capgemini.com>
2018-05-09 11:16:59 +0530

    first commit

--- activity-log.component.ts
+++ activity-log.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivityLogSearch } from './activity-log-search'
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'activity-log',
@@ -7,52 +9,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityLogComponent implements OnInit {
 
+  public model1: string;
+
+  count: number = 25;
+  searchUserName: string = "";
+  fromDate: string;
+  toDate: string;
+  isfromDateInvalid: boolean;
+  istoDateInvalid: boolean;
+  activityLogSearch: ActivityLogSearch;
+
   constructor() { }
-  count:number=25;
-  searchUserName:string="";
+
   ngOnInit() {
-  }
-
-  onExport(){
 
   }
-public model1:string;
+
+  onExport() {
+
+  }
+
   public callBack(newVal1) {
-        console.log('value is changed to ', newVal1);
-        this.model1 = newVal1;
-    }
-  public myNames=['Radha','Shri'];
+    this.model1 = newVal1;
+  }
 
-  public searchCriteriaList=[
-  'year-1985,cause-Blowout',
-  'year-1995,cause-Blowout',
-  'year-1980,cause-Blowout',
-  'year-1990,cause-Blowout',
-  'year-2000,cause-Blowout',
-  'year-1973,cause-Blowout'
+  setfromDate(fromDate: string) {
+    this.fromDate = fromDate;
+  }
+
+  settoDate(toDate: string) {
+    this.toDate = toDate;
+  }
+  checkfromDateFormat(value: boolean) {
+    if (value)
+      this.isfromDateInvalid = false;
+    else
+      this.isfromDateInvalid = true;
+  }
+
+  checktoDateFormat(value: boolean) {
+    if (value)
+      this.istoDateInvalid = false;
+    else
+      this.istoDateInvalid = true;
+  }
+
+  checkDateValidation() {
+
+    if (this.fromDate && this.toDate && !this.isfromDateInvalid && !this.istoDateInvalid) {
+      return !(moment(this.fromDate).isBefore(this.toDate));
+    } else {
+      return false;
+    }
+  }
+
+  public userNames = ['BOG,Amruta', 'CG,Abhishek','WTW,Hemant' ,'BOG,Shweta','CG,Souvik'];
+
+  public searchCriteriaList = [
+    'year-1985,cause-Blowout',
+    'year-1995,cause-Blowout',
+    'year-1980,cause-Blowout',
+    'year-1990,cause-Blowout',
+    'year-2000,cause-Blowout',
+    'year-1973,cause-Blowout'
   ]
 
-  public serverIpList=[
+  public serverIpList = [
     '10.2.402.127',
-  '10.2.402.129',
-  '10.2.402.126',
-  '10.2.402.120',
-  '10.2.402.121',
-  '10.2.402.126'
+    '10.2.402.129',
+    '10.2.402.126',
+    '10.2.402.120',
+    '10.2.402.121',
+    '10.2.402.126'
   ]
   public activityLogs = [
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
-    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135'},
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
+    { date: '21-04-2018 10:00', username: 'BOG, Shah, Sushil', viewmodule: 'Visual Analytics', action: 'Download', details: 'Yearwise view of Incidents', searchCriteria: 'Year - 1985, Cause - Blowout', serverIp: '10.2.402.135' },
 
 
   ]
